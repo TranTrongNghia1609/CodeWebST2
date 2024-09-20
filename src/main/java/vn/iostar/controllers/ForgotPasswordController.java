78b2168ede28e390600bbd1bb11a84ff7f05df1b
Trần Trọng Nghĩa <163334466+TranTrongNghia1609@users.noreply.github.com>
2024-09-20 16:57:40 +0700

    Forgot Password

--- src/main/java/vn/iostar/controllers/ForgotPasswordController.java
+++ src/main/java/vn/iostar/controllers/ForgotPasswordController.java
@@ -11,23 +11,51 @@ import vn.iostar.models.UserModel;
 import vn.iostar.services.IUserService;
 import vn.iostar.services.impl.UserServiceImpl;
 import vn.iostar.ultis.Constant;
+
 @WebServlet(urlPatterns = { "/ForgotPassword" })
-public class ForgotPasswordController extends HttpServlet{
+public class ForgotPasswordController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		req.getRequestDispatcher(Constant.ForgotPassword).forward(req, resp);
-	}			
-	protected void postForgotPassword(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html");
-		resp.setCharacterEncoding("UTF-8");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("UTF-8");
+		// Nhận dữ liệu từ request URL
 		String username = req.getParameter("username");
 		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		String repassword = req.getParameter("repassword");
+		String alertMsg = "";
+		if (!password.equals(repassword)) {
+			alertMsg = "Mật khẩu không khớp!!!";
+			req.setAttribute("alert", alertMsg);
+			req.getRequestDispatcher("/views/ForgotPassword.jsp").forward(req, resp);
+			return;
+		}
+
 		IUserService service = new UserServiceImpl();
 		UserModel user = service.findByUserName(username);
-		if(user.getEmail().equals(email) && user.getUsername().equals(username)) {
-			
-	}
+		if (user != null) {
+			if (!email.equals(user.getEmail())) {
+				alertMsg = "Email không đúng!!!";
+				req.setAttribute("alert", alertMsg);
+				req.getRequestDispatcher("/views/ForgotPassword.jsp").forward(req, resp);
+				return;
+			}
+			service.resetpassword(username, email, password);
+			alertMsg = "Đã đổi mật khẩu thành công,vui lòng đăng nhập";
+			req.setAttribute("alert", alertMsg);
+			req.getRequestDispatcher("/views/ForgotPassword.jsp").forward(req, resp);
+		} else {
+			alertMsg = "Tài khoản không đúng";
+			req.setAttribute("alert", alertMsg);
+			req.getRequestDispatcher("/views/ForgotPassword.jsp").forward(req, resp);
+		}
+
 	}
 }
